ab21ead51f805923a57506f293e12d5b62e6732e
Warut Surapat <wsurapat@gilt.jp>
2019-07-22 19:22:10 +0900

    Add view template.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
-import express from "express";
+import express from 'express';
+import path from 'path';
 const app = express();
 const port = 8080;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.get('/', (request, response) => {
-  response.send("hi dude");
+  response.render('index')
 });
 
 app.listen(port, () => {
